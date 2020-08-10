732b93822b044c95ee263e44f0a442b83bde8eee
Zhijia Zhao <zhaozj13@lenovo.com>
2020-08-10 19:37:58 +0800

    init project

---
+++ source/fizz_buzz.py
@@ -0,0 +1,6 @@
+def fizz_buzz():
+    return ''
+
+
+if __name__ == '__main__':
+    fizz_buzz()
---
+++ tests/test_fizz_buzz.py
@@ -0,0 +1,7 @@
+from source.fizz_buzz import fizz_buzz
+
+
+def test_fizz_buzz():
+    result = fizz_buzz()
+
+    assert result == ''
